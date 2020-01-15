932edc03d9f6ec030d31127ee96c7ba56cbc2082
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-15 10:00:13 +0430

    excel utility library for anar framework

---
+++ src/main/java/af/gov/anar/lib/excel/anarlibexcel/AnarLibExcelApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.excel.anarlibexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/gov/anar/lib/excel/anarlibexcel/AnarLibExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.excel.anarlibexcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarLibExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
